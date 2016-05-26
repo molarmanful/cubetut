e4331d33d4ef4bb3e9ccba16170af54b8ec04092
Ben Pang <benjinpang@gmail.com>
2016-05-25 22:21:15 -0700

    basically done with first draft

---
+++ main.js
@@ -0,0 +1,24 @@
+$('#1').click(function(){
+	$('g-cube').ghighlight('all')
+})
+$('#2').click(function(){
+	$('g-cube').ghighlight('flcross')
+})
+$('#3').click(function(){
+	$('g-cube').ghighlight('fl')
+})
+$('#4').click(function(){
+	$('g-cube').ghighlight('f2l')
+})
+$('#5').click(function(){
+	$('g-cube').ghighlight('eoll')
+})
+$('#6').click(function(){
+	$('g-cube').ghighlight('oll')
+})
+$('#7').click(function(){
+	$('g-cube').ghighlight('coll')
+})
+$('#8').click(function(){
+	$('g-cube').ghighlight('all')
+})
